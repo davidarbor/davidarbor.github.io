872977658a90da96918e5441122a64db46070dc6
David Arbor <davidarbor@students.berkeley.net>
2018-05-17 10:12:27 -0700

    :^)

--- Paul.js
+++ Paul.js
@@ -3,8 +3,14 @@ $(document).ready(function(){
     $('#contentforbirds').hide();
     $('#contentforplants').hide();
     $('#contentforfungi').hide();
-    $('#tilden').html("<img width='500' height='500' src='img/Tilden.png'>");
+    $('#tilden').html("<img width='500' height='500' src='img/Tilden%20Park.png'>");
+    $('#redwood').html("<img width='500' height='500' src='img/Redwood%20Park.png'>");
+    $('#wildcat').html("<img width='500' height='500' src='img/Wildcat%20Park.png'>");
+    $('#claremont').html("<img width='500' height='500' src='img/Claremont%20Canyon%20Park.png'>");
     $('#tilden').hide();
+    $('#redwood').hide();
+    $('#wildcat').hide();
+    $('#claremont').hide();
 
 });
 
@@ -57,15 +63,30 @@ function displayMap(){
     switch(area){
         case "1":
             $('#tilden').show();
+            $('#redwood').hide();
+            $('#wildcat').hide();
+            $('#claremont').hide();
             break;
 
         case "2":
-            return "<img src='img/Aquarius.jpg'>";
+            $('#tilden').hide();
+            $('#redwood').show();
+            $('#wildcat').hide();
+            $('#claremont').hide();
+            break;
 
         case "3":
-            return "<img src='img/Pisces.jpg'>";
+            $('#tilden').hide();
+            $('#redwood').hide();
+            $('#wildcat').show();
+            $('#claremont').hide();
+            break;
 
         case "4":
-            return "<img src='img/Aries.jpg'>";
+            $('#tilden').hide();
+            $('#redwood').hide();
+            $('#wildcat').hide();
+            $('#claremont').show();
+            break;
     }
 }
