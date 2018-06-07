702a2efc72ae7403ce91da96cfe918732c7c5110
David Arbor <davidarbor@students.berkeley.net>
2018-06-07 10:26:43 -0700

    :^)

--- paul.js
+++ paul.js
@@ -109,70 +109,49 @@ function displayMapTwo(){
     }
 }
 
+var tableHeader = ("<tr> <th width='100px'>Kingdom</th> <th width='100px'>Phylum</th> <th width='100px'>Class</th> <th width='100px'>Order</th> <th width='100px'>Family</th><th width='100px'>Genus</th> <th width='100px'>Species</th> <th width='100px'>Common name</th> </tr>");
+
 function fungi(){
     $("body").pagecontainer("change", "#page3", {});
     var $table=$('#displayTable');
+    $table.empty();
+    $table.append(tableHeader);
     for(i=0; i<tildenFungi.length; i++){
-        var kingdom=tildenFungi[i].kingdom;
-        var phylum=tildenFungi[i].phylum;
-        var sclass=tildenFungi[i].class;
-        var order=tildenFungi[i].order;
-        var family=tildenFungi[i].family;
-        var genus=tildenFungi[i].genus;
-        var species=tildenFungi[i].taxon_name;
-        var common=tildenFungi[i].taxon_common_name;
-        var cell=('<td>'+kingdom+'</td><td>'+phylum+'</td><td>'+sclass+'</td><td>'+order+'</td><td>'+family+'</td>'+'</td><td>'+genus+'</td>'+'</td><td>'+species+'</td>'+'</td><td>'+common+'</td>');
-        var row = ('<tr class="data">' + cell + '</tr>');
+        var row=('<tr class="data">'+makeCell(tildenFungi[i].kingdom, tildenFungi[i].phylum, tildenFungi[i].class, tildenFungi[i].order, tildenFungi[i].family, tildenFungi[i].genus, tildenFungi[i].taxon_name, tildenFungi[i].taxon_common_name) + '</tr>');
         $table.append(row);
     }
 }
 function plants(){
     $("body").pagecontainer("change", "#page3", {});
     var $table=$('#displayTable');
+    $table.empty();
+    $table.append(tableHeader);
     for(i=0; i<tildenPlants.length; i++){
-        var kingdom=tildenPlants[i].kingdom;
-        var phylum=tildenPlants[i].phylum;
-        var sclass=tildenPlants[i].class;
-        var order=tildenPlants[i].order;
-        var family=tildenPlants[i].family;
-        var genus=tildenPlants[i].genus;
-        var species=tildenPlants[i].taxon_name;
-        var common=tildenPlants[i].taxon_common_name;
-        var cell=('<td>'+kingdom+'</td><td>'+phylum+'</td><td>'+sclass+'</td><td>'+order+'</td><td>'+family+'</td>'+'</td><td>'+genus+'</td>'+'</td><td>'+species+'</td>'+'</td><td>'+common+'</td>');
-        var row = ('<tr class="data">' + cell + '</tr>');
+        var row=('<tr class="data">'+makeCell(tildenPlants[i].kingdom, tildenPlants[i].phylum, tildenPlants[i].class, tildenPlants[i].order, tildenPlants[i].family, tildenPlants[i].genus, tildenPlants[i].taxon_name, tildenPlants[i].taxon_common_name) + '</tr>');
         $table.append(row);
     }
 }
 function birds(){
     $("body").pagecontainer("change", "#page3", {});
+    var $table=$('#displayTable');
+    $table.empty();
+    $table.append(tableHeader);
     for(i=0; i<tildenBirds.length; i++){
-        var kingdom=tildenBirds[i].kingdom;
-        var phylum=tildenBirds[i].phylum;
-        var sclass=tildenBirds[i].class;
-        var order=tildenBirds[i].order;
-        var family=tildenBirds[i].family;
-        var genus=tildenBirds[i].genus;
-        var species=tildenBirds[i].taxon_name;
-        var common=tildenBirds[i].taxon_common_name;
-        var cell=('<td>'+kingdom+'</td><td>'+phylum+'</td><td>'+sclass+'</td><td>'+order+'</td><td>'+family+'</td>'+'</td><td>'+genus+'</td>'+'</td><td>'+species+'</td>'+'</td><td>'+common+'</td>');
-        var row = ('<tr class="data">' + cell + '</tr>');
+        var row=('<tr class="data">'+makeCell(tildenBirds[i].kingdom, tildenBirds[i].phylum, tildenBirds[i].class, tildenBirds[i].order, tildenBirds[i].family, tildenBirds[i].genus, tildenBirds[i].taxon_name, tildenBirds[i].taxon_common_name) + '</tr>');
         $table.append(row);
     }
 }
 function animals(){
     $("body").pagecontainer("change", "#page3", {});
     var $table=$('#displayTable');
+    $table.empty();
+    $table.append(tableHeader);
     for(i=0; i<tildenOther.length; i++){
-        var kingdom=tildenOther[i].kingdom;
-        var phylum=tildenOther[i].phylum;
-        var sclass=tildenOther[i].class;
-        var order=tildenOther[i].order;
-        var family=tildenOther[i].family;
-        var genus=tildenOther[i].genus;
-        var species=tildenOther[i].taxon_name;
-        var common=tildenOther[i].taxon_common_name;
-        var cell=('<td>'+kingdom+'</td><td>'+phylum+'</td><td>'+sclass+'</td><td>'+order+'</td><td>'+family+'</td>'+'</td><td>'+genus+'</td>'+'</td><td>'+species+'</td>'+'</td><td>'+common+'</td>');
-        var row = ('<tr class="data">' + cell + '</tr>');
+        var row=('<tr class="data">'+makeCell(tildenOther[i].kingdom, tildenOther[i].phylum, tildenOther[i].class, tildenOther[i].order, tildenOther[i].family, tildenOther[i].genus, tildenOther[i].taxon_name, tildenOther[i].taxon_common_name) + '</tr>');
         $table.append(row);
     }
+}
+
+function makeCell(kingdom, phylum, sclass, order,family, genus, species, common){
+    return '<td>'+kingdom+'</td><td>'+phylum+'</td><td>'+sclass+'</td><td>'+order+'</td><td>'+family+'</td>'+'</td><td>'+genus+'</td>'+'</td><td>'+species+'</td>'+'</td><td>'+common+'</td>'
 }
